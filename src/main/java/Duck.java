2e159eacf4e1f4cb18553c2fcd5e7077aabfbd79
Kennethsoh <sgguy26@gmail.com>
2025-01-16 17:21:23 +0800

    level 1

--- src/main/java/Duck.java
+++ src/main/java/Duck.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class Duck {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         System.out.println("_________________________________________________");
         System.out.println("Hello! I'm DUCK");
         System.out.println("What can I do for you?");
         System.out.println("_________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
+        String userInput = scan.nextLine();
+        while (!userInput.equalsIgnoreCase("bye")) {
+            System.out.println("_________________________________________________");
+            System.out.println("     "+userInput);
+            System.out.println("_________________________________________________");
+            userInput = scan.nextLine();
+        }
+        System.out.println("     Bye. Hope to see you again soon!");
         System.out.println("_________________________________________________");
     }
 }
