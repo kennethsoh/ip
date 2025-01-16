06c136da0ab2882a8929fd7d81a2b26af16f6f9a
Kennethsoh <sgguy26@gmail.com>
2025-01-16 16:44:03 +0800

    Level 0

---
+++ src/main/java/Duck.java
@@ -0,0 +1,10 @@
+public class Duck {
+    public static void main(String[] args) {
+        System.out.println("_________________________________________________");
+        System.out.println("Hello! I'm DUCK");
+        System.out.println("What can I do for you?");
+        System.out.println("_________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("_________________________________________________");
+    }
+}
