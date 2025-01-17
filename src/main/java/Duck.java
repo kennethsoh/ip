6dfbef3680068e19af8e6071033b448b1e237bb7
Kennethsoh <sgguy26@gmail.com>
2025-01-17 12:17:05 +0800

    level 2

--- src/main/java/Duck.java
+++ src/main/java/Duck.java
@@ -1,17 +1,29 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duck {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<String>();
         System.out.println("_________________________________________________");
         System.out.println("Hello! I'm DUCK");
         System.out.println("What can I do for you?");
         System.out.println("_________________________________________________");
         String userInput = scan.nextLine();
         while (!userInput.equalsIgnoreCase("bye")) {
-            System.out.println("_________________________________________________");
-            System.out.println("     "+userInput);
-            System.out.println("_________________________________________________");
+            if (userInput.equalsIgnoreCase("list")) {
+                System.out.println("_________________________________________________");
+                int size = list.size();
+                for (int i = 0; i< size; i++) {
+                    System.out.println("     "+(i+1)+". "+list.get(i));
+                }
+                System.out.println("_________________________________________________");
+            } else {
+                System.out.println("_________________________________________________");
+                System.out.println("     added: "+userInput);
+                System.out.println("_________________________________________________");
+                list.add(userInput);
+            }
             userInput = scan.nextLine();
         }
         System.out.println("     Bye. Hope to see you again soon!");
